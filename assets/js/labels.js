840012b3ab0bc5faa65da54ec6fcb74b7874446a
Manoel Afonso <mano.afonso93@gmail.com>
2023-09-08 17:55:32 -0300

    Enhance labels with 2 lines

--- assets/js/labels.js
+++ assets/js/labels.js
@@ -1,41 +1,52 @@
 // just add a new item in that array to add a new label
 const labelsInfo = [
-    { id: 'labelToggle-ps1-classic', class: 'ps1-classic', text: 'SteamStation' },
-    { id: 'labelToggle-ps1-modern', class: 'ps1-modern', text: 'SteamStation' },
-    { id: 'labelToggle-ps2', class: 'ps2', text: 'SteamStation 2' },
-    { id: 'labelToggle-ps3-classic', class: 'ps3-classic', text: 'SteamStation 3' },
-    { id: 'labelToggle-ps3-modern', class: 'ps3-modern', text: 'SS3' },
-    { id: 'labelToggle-gba-classic', class: 'gba-classic', text: 'steam ADVANCE' },
-    { id: 'labelToggle-gba-modern', class: 'gba-modern', text: 'steam ADVANCE' },
-    { id: 'labelToggle-pc-classic', class: 'pc-classic', text: 'PC steam' },
-    { id: 'labelToggle-pc-modern', class: 'pc-modern', text: 'Games for Steam' },
+    { class: 'pc-classic', text: 'PC /// STEAM' },
+    { class: 'pc-modern', text: 'Games /// for Steam' },
+    { class: 'gba-classic', text: 'steam /// ADVANCE' },
+    { class: 'gba-modern', text: 'steam /// ADVANCE' },
+    { class: 'ps1-classic', text: 'SteamStation' },
+    { class: 'ps1-modern', text: 'SteamStation' },
+    { class: 'ps2', text: 'SteamStation 2' },
+    { class: 'ps3-classic', text: 'SteamStation 3' },
+    { class: 'ps3-modern', text: 'SS3' },
 ];
 
 function setupToggles(labelsInfo) {
     const topBar = document.getElementById('topBar');
     const inputContainer = document.querySelector('.input-container');
+    const linef = document.querySelector('.top-bar #linef');
+    const lines = document.querySelector('.top-bar #lines');
 
     labelsInfo.forEach(label => {
-        document.getElementById(label.id).addEventListener('change', function () {
+        document.getElementById('labelToggle-' + label.class).addEventListener('change', function () {
             const wasChecked = this.checked;
 
             labelsInfo.forEach(label => {
-                document.getElementById(label.id).checked = false;
+                document.getElementById('labelToggle-' + label.class).checked = false;
                 topBar.classList.remove(label.class);
+                linef.classList.remove(label.class + '-linef');
+                lines.classList.remove(label.class + '-lines');
             });
 
             if (!wasChecked) {
                 topBar.style.visibility = "hidden";
                 inputContainer.style.display = 'none';
-                topBar.textContent = "";
-                customTextLabel.value = "";
+                topBar.style.visibility = "hidden";
+                linef.textContent = '';
+                lines.textContent = '';
+                customTextLabel.value = '';
             } else {
                 this.checked = true;
                 topBar.classList.add(label.class);
+                linef.classList.add(label.class + '-linef');
+                lines.classList.add(label.class + '-lines');
                 topBar.style.visibility = "visible";
                 inputContainer.style.display = 'flex';
-                topBar.textContent = label.text;
                 customTextLabel.value = label.text;
+
+                let labelLines = label.text.split('///');
+                linef.textContent = labelLines[0] || '';
+                lines.textContent = labelLines[1] || '';
             }
         });
     });
@@ -44,8 +55,19 @@ function setupToggles(labelsInfo) {
 setupToggles(labelsInfo);
 
 document.getElementById('customTextLabel').addEventListener('input', function () {
-    let lines = this.value.split('\n');
-    
-    document.querySelector('.top-bar .linef').textContent = lines[0] || '';
-    document.querySelector('.top-bar .lines').textContent = lines[1] || '';
+    let lines = this.value.split('///');
+
+    let linef = document.querySelector('.top-bar #linef');
+    let linesElem = document.querySelector('.top-bar #lines');
+
+    linef.textContent = lines[0] || '';
+    linesElem.textContent = lines[1] || '';
+
+    if (lines[0] && !lines[1] || !lines[0] && lines[1]) {
+        linef.classList.add('single-line');
+        linesElem.classList.add('single-line');
+    } else {
+        linef.classList.remove('single-line');
+        linesElem.classList.remove('single-line');
+    }
 });
